64da1a1b587eab6371c06e9ea6f043847ef31526
Ibrahim Saqr <ebrahimes@gmail.com>
2015-12-18 19:43:17 +0200

    start tiesting

--- smarcApp/www/js/scripts/controllers/homeController.js
+++ smarcApp/www/js/scripts/controllers/homeController.js
@@ -2,32 +2,10 @@ smarc.controller('homeController', [
 	'$scope',
 	'$http',
 	function($scope, $http){
-	    $scope.allNodes = function(){
+	    $scope.toggle = function(id){
 	    	$http({
 	    		method: "GET",
-	    		url: "http://localhost:3050/light/allNodes",
-	    	}).then(function(data){
-	    		console.log(data);
-	    	});
-	    };
-	    $scope.onLight = function(id){
-	    	$http({
-	    		method: "GET",
-	    		url: "http://localhost:3050/light/on/:id",
-	    		data: {
-	    			id: id
-	    		}
-	    	}).then(function(data){
-	    		console.log(data);
-	    	});
-	    };
-	    $scope.offLight = function(id){
-	    	$http({
-	    		method: "GET",
-	    		url: "http://localhost:3050/light/off/:id",
-	    		data: {
-	    			id: id
-	    		}
+	    		url: "http://localhost:3050/light/toggle/" + id
 	    	}).then(function(data){
 	    		console.log(data);
 	    	});
