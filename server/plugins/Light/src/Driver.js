3cb7a9ceb5731dbe3e3a72c8c2bf34ca6a6ebdc0
Foxawy <xfoxawy@gmail.com>
2016-06-04 15:51:44 +0200

    still trying stuff out

--- server/plugins/Light/src/Driver.js
+++ server/plugins/Light/src/Driver.js
@@ -11,6 +11,9 @@ var Driver = function(Core){
 	var model = "light";
 	var db = Core.db;
 	var publisherClient = Core.redis.createClient();
+	var reconnectionInterval = 200; // reconnection to dead nodes interval
+	var maxTries = 10 ; // reconnection to dead nodes max tries
+	
 	// all nodes placeholder
 	this.nodes = [];
 	// all errors placeholder
@@ -19,7 +22,11 @@ var Driver = function(Core){
 	this.mappedPoints = [];
 	// hold all rooms in array
 	this.rooms = [];
-	// connect
+	// holds all dead nodes
+	this.deadNodes = [];
+
+
+	// connect ready nodes in db
 	(function connectNodes(){
 			// load nodes from database
 			loadNodes(function(nodes){
@@ -33,6 +40,18 @@ var Driver = function(Core){
 				self.rooms = rooms;
 			})
 	}());
+
+	// interval to check if there any dead nodes and try to reconnect to them
+	setInterval(reConnect, reconnectionInterval);
+
+	// responsible of reconnection for dead nodes 
+	function reConnect()
+	{
+		if(self.deadNodes.length)
+		{
+			console.log("there is some dead nodes overe here");
+		}
+	}
 
 	// function load nodes from db
 	function loadNodes(cb){
